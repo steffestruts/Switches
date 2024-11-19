031d2bc20c9d05ad5bb860af0cf776d298e84bd0
Stefan Sjöberg <bird.nerd@outlook.com>
2024-11-19 13:49:40 +0100

    Add project files.

---
+++ Switches/Program.cs
@@ -0,0 +1,19 @@
+﻿string option = "2";
+
+switch (option.ToLower())
+{
+	case "1":
+        Console.WriteLine("Alternativ 1 körs");
+        break;
+    case "2":
+        Console.WriteLine("Alternativ 2 körs");
+        break;
+    case "3":
+        Console.WriteLine("Alternativ 3 körs");
+        break;
+    default:
+        Console.WriteLine("Standard körs");
+        break;
+}
+
+Console.ReadKey();
